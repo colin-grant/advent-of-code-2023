0753e4bacb54b846dbc4600c0218cd7c2da1ab8d
Colin Grant <63599384+colin-grant@users.noreply.github.com>
2023-12-02 11:21:16 +0000

    Day 2 completed

--- com/adventofcode/colingrant/Main.java
+++ com/adventofcode/colingrant/Main.java
@@ -1,7 +1,7 @@
 package com.adventofcode.colingrant;
 
-import com.adventofcode.colingrant.challenges.Day1;
-import com.adventofcode.colingrant.challenges.Day2;
+import com.adventofcode.colingrant.challenges.*;
+import com.adventofcode.colingrant.common.Challenge;
 import com.adventofcode.colingrant.common.InputFileReader;
 
 public class Main 
@@ -9,19 +9,11 @@ public class Main
     public static void main(String[] args) 
     {
         // Run one of the challenges.
-        day2(); 
+        runChallenge(new Day2(), "day2.txt"); 
     }
 
-    private static void day1()
+    public static void runChallenge(Challenge challenge, String inputFile)
     {
-        Day1 day1 = new Day1(); 
-        day1.run(InputFileReader.readInputFile("day1.txt"));
+        challenge.run(InputFileReader.readInputFile(inputFile));
     }
-
-    private static void day2()
-    {
-        Day2 day2 = new Day2(); 
-        day2.run(InputFileReader.readInputFile("day2.txt"));
-    }
-
 }
--- com/adventofcode/colingrant/challenges/Day2.java
+++ com/adventofcode/colingrant/challenges/Day2.java
@@ -1,5 +1,9 @@
 package com.adventofcode.colingrant.challenges;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.adventofcode.colingrant.common.Challenge;
 import com.adventofcode.colingrant.common.Input;
 
@@ -9,68 +13,115 @@ public class Day2 implements Challenge
     public final int VALID_GREEN_COUNT = 13;
     public final int VALID_BLUE_COUNT = 14;
 
+    //
+    // After part 2 was revealed I wrote a parser for the file. 
+    // This stores all the games together with the maximum red, green and blue cube
+    // counts, since this is all we need for all games. 
+    //
+    private static class Game
+    {
+        private int id; 
+        private Map<String, Integer> colourToMaxCount = new HashMap<>(); 
+
+        public Game(int id)
+        {
+            this.id = id ;
+            colourToMaxCount.put("red", 0);
+            colourToMaxCount.put("green",0); 
+            colourToMaxCount.put("blue", 0); 
+        }
+
+        public void setCount(String colour, int count)
+        {
+            Integer maxCount = colourToMaxCount.get(colour); 
+            if ( maxCount != null )
+            {
+                if ( count > maxCount )
+                {
+                    colourToMaxCount.put(colour, count); 
+                }
+            }
+        }
+
+        public int getMaxCount(String colour)
+        {
+            return colourToMaxCount.get(colour); 
+        }
+
+        public int getId()
+        {
+            return id; 
+        }
+    }
+
+    private static class GameParser 
+    {
+        public static ArrayList<Game> parseInput( Input input ) 
+        {
+            ArrayList<Game> gameList = new ArrayList<>();
+
+            for ( String line : input )
+            {
+                String idAndCubeCounts[] = line.split(":"); 
+
+                String idStr = idAndCubeCounts[0].substring("Game ".length());
+                int id = Integer.parseInt(idStr);
+                
+                Game game = new Game(id); 
+
+                String games[] = idAndCubeCounts[1].split(";"); 
+
+                for ( String gameInput : games )
+                {
+                    String cubes[] = gameInput.split(",");
+                    for ( String cubeCount : cubes )
+                    {
+                        String[] countAndColour = cubeCount.trim().split(" "); 
+                        int count = Integer.parseInt(countAndColour[0]);
+                        String colour = countAndColour[1].trim(); 
+
+                        game.setCount(colour, count); 
+                    }
+                }
+                gameList.add(game); 
+            }
+            return gameList; 
+        }
+    }
+
+
     @Override
     public void run(Input input) 
     {
-        // Rough and ready initially - until we see what part 2 brings. 
-        int idCount = 0 ;
+        ArrayList<Game> games = GameParser.parseInput(input); 
+        part1(games);
+        part2(games); 
+    }
 
-        for ( String line : input )
+    private void part1(ArrayList<Game> games)
+    {
+        int idTotal = 0 ;
+
+        for ( Game game : games )
         {
-            boolean isValid = true; 
-
-            String split1[] = line.split(":"); 
-
-            String idStr = split1[0].substring("Game ".length());
-            int id = Integer.parseInt(idStr); 
-            String games[] = split1[1].split(";"); 
-            for ( String game : games )
+            if (    (game.getMaxCount("red") <= VALID_RED_COUNT)
+                &&  (game.getMaxCount("green") <= VALID_GREEN_COUNT)
+                &&  (game.getMaxCount("blue") <= VALID_BLUE_COUNT)  )
             {
-                String cubes[] = game.split(",");
-                for ( String cubeCount : cubes )
-                {
-                    String[] countAndColour = cubeCount.trim().split(" "); 
-                    int count = Integer.parseInt(countAndColour[0]);
-                    String colour = countAndColour[1].trim(); 
-
-                    switch( colour )
-                    {
-                        case "red":
-                            if ( count > VALID_RED_COUNT )
-                            {
-                                isValid = false; 
-                            }
-                            break;
-                        case "green":
-                            if ( count > VALID_GREEN_COUNT )
-                            {
-                                isValid = false; 
-                            }
-                            break;
-                        case "blue":
-                            if ( count > VALID_BLUE_COUNT )
-                            {
-                                isValid = false;
-                            }
-                            break; 
-                    }
-
-                    if ( !isValid )
-                    {
-                        break; 
-                    }
-                }
-
-                if ( !isValid )
-                {
-                    break; 
-                } 
-            }
-            if ( isValid )
-            { 
-                idCount += id; 
+                idTotal += game.getId(); 
             }
         }
-        System.out.println("ID count = " + idCount); 
+
+        System.out.println("Part1: ID Total = " + idTotal);
+    }
+
+    private void part2(ArrayList<Game> games) 
+    {
+        int powerTotal = 0; 
+        for ( Game game : games )
+        {
+            powerTotal += (game.getMaxCount("red") * game.getMaxCount("green") * game.getMaxCount("blue"));
+        }
+        System.out.println("Part2: power total = " + powerTotal);
     }
 }
